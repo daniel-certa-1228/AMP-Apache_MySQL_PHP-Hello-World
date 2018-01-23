4f1b2260812ca2882980ac076dce4ace0a8c8239
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-23 17:18:02 -0600

    logic bolck

--- index.php
+++ index.php
@@ -4,5 +4,25 @@
     $con->close(); // close db connection
     echo "<p>$message</p>";
     echo "Hello From Sites Folder!";
+?>
+
+<!-- PHP Logic block with HTML -->
+<?php
+    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+?>
+    <h3>strpos() must have returned non-false</h3>
+    <p>You are using Internet Explorer</p>
+<?php
+    } else {
+?>
+    <h3>strpos() must have returned false</h3>
+    <p>You are not using Internet Explorer</p>
+<?php
+    }
+?>
+<!-- END PHP Logic block -->
+
+<?php
     phpinfo(); // phpinfo() is commonly used to check configuration settings and for available predefined variables on a given system.
 ?>
+
