c6cc2376380d24bde06425441fc369d80631c22e
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-23 16:04:16 -0600

    comments

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-    $con = new mysqli("127.0.0.1", "root", "password", "mydb");
-    $message = $con->query("SELECT message FROM myTable")->fetch_object()->message;
-    $con->close();
+    $con = new mysqli("127.0.0.1", "root", "password", "mydb"); //new database connection, params are (host, name, password, db name)
+    $message = $con->query("SELECT message FROM myTable")->fetch_object()->message; // SQL query to get data
+    $con->close(); // close db connection
     echo "<p>$message</p>";
     echo "Hello From Sites Folder!";
-    phpinfo();
+    phpinfo(); // phpinfo() is commonly used to check configuration settings and for available predefined variables on a given system.
 ?>
